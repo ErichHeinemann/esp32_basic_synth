681e1ebd02ed3afb9bb417ed95859f0dde699b98
Erich <35889973+ErichHeinemann@users.noreply.github.com>
2021-05-27 14:12:35 +0200

    Update i2s_interface.ino

--- i2s_interface.ino
+++ i2s_interface.ino
@@ -3,6 +3,7 @@
  *
  * Author: Marcel Licence
  */
+
 #include <driver/i2s.h>
 
 /*
@@ -11,16 +12,9 @@
  */
 //#define I2S_NODAC
 
-/*
- * Define and connect your PINS to DAC here
- */
-#define I2S_BCLK_PIN	25
-#define I2S_WCLK_PIN	27
-#define I2S_DOUT_PIN	26
 
 
 const i2s_port_t i2s_num = I2S_NUM_0; // i2s port number
-
 
 #ifdef I2S_NODAC
 
@@ -45,9 +39,9 @@ bool i2s_write_sample(uint32_t sample)
 static uint32_t i2sACC;
 static uint16_t err;
 
-bool writeDAC(float DAC_f)
+bool i2s_write_stereo_samples(float *fl_sample, float *fr_sample)
 {
-    uint16_t DAC = 0x8000 + int16_t(DAC_f * 32767.0f);
+    uint16_t DAC = 0x8000 + int16_t(*fl_sample * 32767.0f);
     for (uint8_t i = 0; i < 32; i++)
     {
         i2sACC = i2sACC << 1;
@@ -68,7 +62,8 @@ bool writeDAC(float DAC_f)
 
 #else
 
-bool i2s_write_sample_32ch2(uint64_t sample ){
+bool i2s_write_sample_32ch2(uint64_t sample)
+{
     static size_t bytes_written = 0;
     i2s_write((i2s_port_t)i2s_num, (const char *)&sample, 8, &bytes_written, portMAX_DELAY);
 
@@ -82,21 +77,74 @@ bool i2s_write_sample_32ch2(uint64_t sample ){
     }
 }
 
-#endif
 
-inline
-bool i2s_write_samples(float left, float right ){
-    static union sampleTUNT{
+bool i2s_write_stereo_samples(float *fl_sample, float *fr_sample)
+{
+
+#ifdef SAMPLE_SIZE_24BIT
+#if 0
+    static union sampleTUNT
+    {
+        uint8_t sample[8];
+        int32_t ch[2];
+    } sampleDataU;
+#else
+    static union sampleTUNT
+    {
+        int32_t ch[2];
+        uint8_t bytes[8];
+    } sampleDataU;
+#endif
+#endif
+#ifdef SAMPLE_SIZE_16BIT
+    static union sampleTUNT
+    {
+        uint32_t sample;
+        int16_t ch[2];
+    } sampleDataU;
+#endif
+#ifdef SAMPLE_SIZE_32BIT
+    static union sampleTUNT
+    {
         uint64_t sample;
         int32_t ch[2];
     } sampleDataU;
+#endif
 
-    sampleDataU.ch[0] = int32_t(left * 536870911.0f);
-    sampleDataU.ch[1] = int32_t(right * 536870911.0f);
+    /*
+     * using RIGHT_LEFT format
+     */
+#ifdef SAMPLE_SIZE_16BIT
+    sampleDataU.ch[0] = int16_t(*fr_sample * 16383.0f); /* some bits missing here */
+    sampleDataU.ch[1] = int16_t(*fl_sample * 16383.0f);
+#endif
+#ifdef SAMPLE_SIZE_32BIT
+    sampleDataU.ch[0] = int32_t(*fr_sample * 1073741823.0f); /* some bits missing here */
+    sampleDataU.ch[1] = int32_t(*fl_sample * 1073741823.0f);
+#endif
 
-    return i2s_write_sample_32ch2( sampleDataU.sample );
+    static size_t bytes_written = 0;
+    static size_t bytes_read = 0;
+
+#ifdef SAMPLE_SIZE_16BIT
+    i2s_write(i2s_num, (const char *)&sampleDataU.sample, 4, &bytes_written, portMAX_DELAY);
+#endif
+#ifdef SAMPLE_SIZE_32BIT
+    i2s_write(i2s_num, (const char *)&sampleDataU.sample, 8, &bytes_written, portMAX_DELAY);
+#endif
+
+    if (bytes_written > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
+
+#endif
 
 
 /*
@@ -112,7 +160,12 @@ i2s_config_t i2s_config =
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
     .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_I2S_MSB,
 #else
+#ifdef SAMPLE_SIZE_16BIT
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+#endif
+#ifdef SAMPLE_SIZE_32BIT
     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+#endif
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
     .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
 #endif
@@ -128,21 +181,29 @@ i2s_pin_config_t pins =
 {
     .bck_io_num = I2S_PIN_NO_CHANGE,
     .ws_io_num =  I2S_PIN_NO_CHANGE,
-    .data_out_num = 22,
+    .data_out_num = I2S_NODAC_OUT_PIN,
     .data_in_num = I2S_PIN_NO_CHANGE
 };
 #else
 i2s_pin_config_t pins =
 {
+#ifdef ESP32_AUDIO_KIT
+    .bck_io_num = IIS_SCLK,
+    .ws_io_num =  IIS_LCLK,
+    .data_out_num = IIS_DSIN,
+    .data_in_num = IIS_DSOUT
+#else
     .bck_io_num = I2S_BCLK_PIN,
     .ws_io_num =  I2S_WCLK_PIN,
     .data_out_num = I2S_DOUT_PIN,
     .data_in_num = I2S_PIN_NO_CHANGE
+#endif
 };
 #endif
 
 
-void setup_i2s(){
+void setup_i2s()
+{
     i2s_driver_install(i2s_num, &i2s_config, 0, NULL);
     i2s_set_pin(I2S_NUM_0, &pins);
     i2s_set_sample_rates(i2s_num, SAMPLE_RATE);
