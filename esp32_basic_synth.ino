3c2e6c920ce477a183d9218ee48d07da6db2c0c3
Erich <35889973+ErichHeinemann@users.noreply.github.com>
2021-05-27 14:05:35 +0200

    Update esp32_basic_synth.ino

--- esp32_basic_synth.ino
+++ esp32_basic_synth.ino
@@ -4,76 +4,17 @@
  *
  * Author: Marcel Licence
  */
- /* 
-  *  2021-04-20 E.Heinemann, replaced custom MIDI Code the the Arduino MIDI Library 
-  *  2021-04-21 E.Heinemann, added Pitchbend-Control
-*/
 
+#include "config.h"
 
 /*
  * required include files
  */
 #include <arduino.h>
-#include <driver/i2s.h>
-#include <MIDI.h>
 #include <WiFi.h>
 
-
-
-/*
- * You can modify the sample rate as you want
- */
-#define SAMPLE_RATE	48000
-
-
-// #define ANALOG_INPUTS  // If this is defined, the system will use analog inputs of ADC 1 and ADC2 to react on inputs in the range of 0 - 2.5Volts!!
-
-
-#ifdef ANALOG_INPUTS
-volatile uint8_t analog_prescaler = 0;
-uint8_t gpio_playbackspeed = 32; // Good GPIO-Ports on ADC1 with 12 Bit Resolution, max input is 2.5Volts. To use them with 5 Volts, please use 2 or more resistors to split the level. and protect the inputs from getting more than 3.3volts!!
-uint8_t gpio_bitcrush =      33;
-uint8_t gpio_biCutoff =      34;
-uint8_t gpio_biReso   =      35;
-
-uint8_t tmp_playbackspeed; // The Playbackspeed
-uint8_t tmp_bitcrush;      // is bitcrusher active
-uint8_t tmp_biCutoff;      // Cutoff-Frequency of the filter
-uint8_t tmp_biReso;        // Resonance of the filter
-
-uint8_t last_playbackspeed; // The Playbackspeed
-uint8_t last_bitcrush;      // is bitcrusher active
-uint8_t last_biCutoff;      // Cutoff-Frequency of the filter
-uint8_t last_biReso;        // Resonance of the filter
-
-#endif
-
-
-// These values are only used to make an integration with MIDI, additional analog inputs or with an Menü slightly simplier
-// The values could be 0 - 127 or floats ... 
-
-uint8_t global_playbackspeed; // The Playbackspeed
-uint8_t global_bitcrush;      // is bitcrusher active
-uint8_t global_biCutoff;      // Cutoff-Frequency of the filter
-uint8_t global_biReso;        // Resonance of the filter
-
-
-// Pitchbend
-float globalBend = 1.0;
-float globalModulation = 1.0; // like Bend a global value which could go from 0.95 to 1.05 via Sinus
-
-// float global env_mod = 1;  // env to pitch? 
-
-
-
-/*
- * this is more an experiment required for other data formats
- */
-union sampleTUNT
-{
-    uint64_t sample;
-    int32_t ch[2];
-} sampleDataU;
+/* this is used to add a task to core 0 */
+TaskHandle_t  Core0TaskHnd ;
 
 
 void setup()
@@ -84,11 +25,30 @@ void setup()
     delay(500);
 
     Serial.begin(115200);
+
     Serial.println();
+
+    Serial.printf("Initialize Reverb Module\n");
+    Reverb_Setup();
+    
+    Serial.printf("Initialize Delay Module\n");
+    Delay_Init();
+
     Serial.printf("Initialize Synth Module\n");
     Synth_Init();
     Serial.printf("Initialize I2S Module\n");
+
+    // Reverb_Setup();
+
+    Blink_Setup();
+
+#ifdef ESP32_AUDIO_KIT
+    ac101_setup();
+#endif
+
     setup_i2s();
+
+
 
     Serial.printf("Initialize Midi Module\n");
     Midi_Setup();
@@ -105,6 +65,9 @@ void setup()
     // esp_wifi_deinit();
 #endif
 
+
+
+
     Serial.printf("ESP.getFreeHeap() %d\n", ESP.getFreeHeap());
     Serial.printf("ESP.getMinFreeHeap() %d\n", ESP.getMinFreeHeap());
     Serial.printf("ESP.getHeapSize() %d\n", ESP.getHeapSize());
@@ -113,10 +76,58 @@ void setup()
     Serial.printf("Firmware started successfully\n");
 
 #if 0 /* activate this line to get a tone on startup to test the DAC */
-    Synth_NoteOn(64);
+    Synth_NoteOn(0, 64, 1.0f);
+#endif
+
+#ifdef ADC_TO_MIDI_ENABLED
+    xTaskCreatePinnedToCore(Core0Task, "Core0Task", 8000, NULL, 999, &Core0TaskHnd, 0);
 #endif
 }
 
+
+void Core0TaskSetup()
+{
+    /*
+     * init your stuff for core0 here
+     */
+#ifdef ADC_TO_MIDI_ENABLED
+    AdcMul_Init();
+#endif
+
+
+// Init I2C
+
+}
+
+void Core0TaskLoop()
+{
+    /*
+     * put your loop stuff for core0 here
+     */
+
+// Request Data from I2C
+
+
+// Update LEDs via I2C
+     
+#ifdef ADC_TO_MIDI_ENABLED
+    AdcMul_Process();
+#endif
+}
+
+void Core0Task(void *parameter)
+{
+    Core0TaskSetup();
+
+    while (true)
+    {
+        Core0TaskLoop();
+
+        /* this seems necessary to trigger the watchdog */
+        delay(1);
+        yield();
+    }
+}
 
 /*
  * use this if something should happen every second
@@ -124,7 +135,7 @@ void setup()
  */
 inline void Loop_1Hz(void)
 {
-
+    Blink_Process();
 }
 
 
@@ -133,89 +144,55 @@ inline void Loop_1Hz(void)
  * - all is done in a blocking context
  * - do not block the loop otherwise you will get problems with your audio
  */
-void loop(){
+float fl_sample, fr_sample;
+
+void loop()
+{
     static uint32_t loop_cnt_1hz;
     static uint8_t loop_count_u8 = 0;
 
     loop_count_u8++;
 
     loop_cnt_1hz ++;
-    if (loop_cnt_1hz >= SAMPLE_RATE){
+    if (loop_cnt_1hz >= SAMPLE_RATE)
+    {
         Loop_1Hz();
+        loop_cnt_1hz = 0;
     }
 
 #ifdef I2S_NODAC
-    if( writeDAC(l_sample)){
+    if (writeDAC(l_sample))
+    {
         l_sample = Synth_Process();
     }
 #else
-   /* function returns always true / it blocks until samples are written to buffer */
-    if( i2s_write_sample_32ch2(sampleDataU.sample)){
-        float fl_sample, fr_sample;
-        Synth_Process(&fl_sample, &fr_sample);
-        sampleDataU.ch[0] = int32_t(fl_sample * 536870911.0f);
-        sampleDataU.ch[1] = int32_t(fr_sample * 536870911.0f);
+
+    if (i2s_write_stereo_samples(&fl_sample, &fr_sample)){
+        /* nothing for here */
     }
+    Synth_Process(&fl_sample, &fr_sample);
+    
+    /*
+     * add also some reverb
+     * 
+     * 
+     */
+    // rocess Reverb
+    Reverb_Process( &fl_sample, &fr_sample, SAMPLE_BUFFER_SIZE );
+
+    /*
+     * process delay line
+     */
+     Delay_Process(&fl_sample, &fr_sample);
+    
+
 #endif
 
-
-    LFO_calc();
-    
     /*
      * Midi does not required to be checked after every processed sample
      * - we divide our operation by 8
      */
-    if( loop_count_u8 % 8 == 0 ){
-         MIDI_Process();
+    if (loop_count_u8 % 8 == 0){
+        Midi_Process();
     }
-
-
-
-#ifdef ANALOG_INPUTS
-    // The valuees could be changed by Potentiometers or by MIDI
-    analog_prescaler++;
-    if( analog_prescaler > 23 ){
-
-    
-      
-     tmp_playbackspeed = floor( analogRead( gpio_playbackspeed ) /32 );
-     tmp_bitcrush = floor( analogRead( gpio_bitcrush ) /32 );
-     tmp_biCutoff = floor( analogRead( gpio_biCutoff ) /32 );
-     tmp_biReso = floor( analogRead( gpio_biReso ) /32 );
-               
-     if( tmp_playbackspeed !=  last_playbackspeed ){
-        last_playbackspeed = tmp_playbackspeed;
-        global_playbackspeed = tmp_playbackspeed;
-        Sampler_SetPlaybackSpeed( NORM127MUL * global_playbackspeed );
-     }  
-     if( tmp_bitcrush !=  last_bitcrush ){
-        last_bitcrush = tmp_bitcrush;
-        global_bitcrush = tmp_bitcrush;
-        // Effect_SetBitCrusher( NORM127MUL * global_bitcrush )
-     }  
-     if( tmp_biCutoff !=  last_biCutoff ){
-        last_biCutoff = tmp_biCutoff;
-        global_biCutoff = tmp_biCutoff;
-        // Effect_SetBiCutoff( NORM127MUL * global_biCutoff )
-     }  
-     if( tmp_biReso !=  last_biReso ){
-        last_biReso = tmp_biReso;
-        global_biReso = tmp_biReso;
-        // Effect_SetBiReso( NORM127MUL * global_biReso );
-     }
-
-    // ADD here your favorite things
-    // trigger samples via buttons or control-voltagee or do something else here.
-    // request data from touch-sensors?
-
-    
-     
-     analog_prescaler = 0;
-    }
-    
-#endif
-
-
-
-
 }
